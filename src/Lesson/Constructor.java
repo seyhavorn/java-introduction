9a560776038ca8471e68f05b2663d3decb68a85d
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2023-10-09 16:57:52 +0700

    Update class Counter>

--- src/Lesson/Constructor.java
+++ src/Lesson/Constructor.java
@@ -27,3 +27,20 @@ public class Constructor {
         s2.display();
     }
 }
+
+class Counter {
+    static int count = 0;
+
+    Counter() {
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+    }
+}
+
+//Java Object Class:
